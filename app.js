64fac3d13ff264c1e6af620294b838f967cad65e
Clarity-89 <homes89@ukr.net>
2015-11-03 20:24:14 +0200

    Filter news articles into top rated and the rest

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ $(document).ready(function () {
     //get the data
     $.getJSON('http://www.freecodecamp.com/news/hot').then(
         function (data) {
-            console.log('Success', '\n', data);
+            console.log('Success');
+            //Create lit of headlines with 20 or more than upvotes
+            var topRated = data.filter(function (el) {
+                return el.rank >= 20;
+            });
+
+            var rest = data.filter(function (el) {
+                return topRated.indexOf(el) == -1;
+            });
+            console.log('rest', rest.length);
         },
         function () {
             console.log('Request failed');
